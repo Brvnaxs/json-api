97216c167b3619567c1cde7ad5d2076344a00576
AlanSilva <aluno@naverecife.local>
2023-05-31 19:41:17 -0300

    Add cors

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const salaDeEstarRoutes = require('./routes/salaDeEstarRoutes');
 const quartoRoutes = require('./routes/quartoRoutes');
 const cozinhaRoutes = require('./routes/cozinhaRoutes');
 const banheiroRoutes = require('./routes/banheiroRoutes');
+const cors = require('cors')
 const App = express();
 
 App.use(express.json());
 App.use(express.urlencoded({ extended: true }));
+App.use(cors)
 
 App.use(produtosRoutes);
 App.use(categoriasRoutes);
